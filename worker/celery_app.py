60306a2055bc52bd14dcef97ffa33589ea3e19af
lxrdrgnbrn <ilyaivanov12345@gmail.com>
2026-01-22 01:43:49 +0900

    feat: add observability stack (prometheus + grafana + metrics)

--- worker/celery_app.py
+++ worker/celery_app.py
@@ -1,10 +1,12 @@
 import os
 from celery import Celery
+from celery.signals import worker_ready
+from prometheus_client import start_http_server
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 celery_app = Celery(
-    'worker',
+    "worker",
     broker=REDIS_URL,
     backend=REDIS_URL,
     include=['worker.tasks']
@@ -15,5 +17,15 @@ celery_app.conf.update(
     accept_content=["json"],
     result_serializer="json",
     timezone="UTC",
-    enable_utc=True
+    enable_utc=True,
+    worker_pool='solo' 
 )
+
+@worker_ready.connect
+def start_metrics_server(sender, **kwargs):
+    try:
+        port = 8001
+        start_http_server(port)
+        print(f"[Monitoring] Metrics server started on port {port}")
+    except Exception as e:
+        print(f"[Monitoring] Failed to start metrics server: {e}")
--- worker/tasks.py
+++ worker/tasks.py
@@ -1,6 +1,8 @@
 from worker.celery_app import celery_app
 from worker.model import ObjectDetector
+from worker.monitoring import TASKS_PROCESSED, OBJECTS_DETECTED, PROCESSING_TIME
 import os
+import time
 
 detector = None
 
@@ -10,17 +12,30 @@ def process_image_task(image_path, output_path):
     if detector is None:
         print("Loading model inside worker...")
         detector = ObjectDetector()
+    
+    start_time = time.time()
+    
     try:
         if not os.path.exists(image_path):
             return {"error": f"file {image_path} not found"}
         
-        stats = detector.predict(image_path, output_path)
+        # Инференс
+        detected_objects = detector.predict(image_path, output_path)
+        
+        # --- МЕТРИКИ ---
+        TASKS_PROCESSED.inc() 
+        
+        for obj_name in detected_objects:
+            OBJECTS_DETECTED.labels(class_name=obj_name).inc()
+            
+        duration = time.time() - start_time
+        PROCESSING_TIME.observe(duration)
+        # ----------------
         
         return {
             "status": "success",
-            "input": image_path,
-            "output": output_path,
-            "objects": stats
+            "objects": detected_objects,
+            "duration": duration
         }
     except Exception as e:
         return {"status": "error", "message": str(e)}
